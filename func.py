7987132359ee13c400535df70a779c8e2cddb764
Boulkhessaim Djalel <90778385+FrozenOption@users.noreply.github.com>
2022-10-04 22:26:40 +0000

    added silent flag and fixed some logic issues

--- func.py
+++ func.py
@@ -1,4 +1,4 @@
-import requests, re, sys, os
+import requests, sys, os
 from urllib.parse import urlparse
 from colorama import init, Fore, Back, Style
 
@@ -6,7 +6,7 @@ def path_only(url):
 	final_joined = []
 	parts = url.split('/')
 	joined = ['/'.join(parts[:p+1]) for p in range(2,len(parts))]
-	for join in joined:
+	for join in joined[:-1]:
 		final_joined.append(join + "PAYLOAD")
 	return final_joined
 
@@ -26,9 +26,10 @@ def params_only(url):
 		final_query.append("%s://%s%s?%s" % (str(parsed.scheme),str(parsed.netloc),str(parsed.path),query))
 	return final_query
 
-def attack(url):
+def attack(url,s):
 	requested = requests.get(url).text
 	if ("root:" or "boot") in requested:
-		print(Fore.GREEN + "[+] " + url)
-	else:
+		print(Fore.GREEN + "[+] " + url + "\t\t[ VULNERABLE! ]")
+	elif (("root:" or "boot") not in requested) and (s==False):
 		print(Fore.RED + "[-] " + url)
+
